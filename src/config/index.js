03322cbaac70880a17c731789a832f2167c9c5d0
alexs <alex_ok@hotmail.it>
2018-11-25 16:29:51 +0100

    config fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
 export const google_client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID
 
-export const aws_exports = JSON.parse(process.env.REACT_APP_AWS_EXPORTS) || require('../aws-exports').default
+export const aws_exports = process.env.REACT_APP_AWS_EXPORTS
+  ? JSON.parse(process.env.REACT_APP_AWS_EXPORTS)
+  : null
